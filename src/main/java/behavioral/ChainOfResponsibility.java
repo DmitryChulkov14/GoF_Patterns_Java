e996afa4e721ae9725cad4a0ad43a9245931f32c
DmitryChulkov14 <dimon_krem@mail.ru>
2021-07-25 17:21:15 +0300

    ChainOfResponsibility

---
+++ src/main/java/behavioral/ChainOfResponsibility.java
@@ -0,0 +1,98 @@
+package behavioral;
+
+/**
+ * Chain of responsibility design pattern gives more than one object an opportunity
+ * to handle a request by linking receiving objects together in form of a chain.
+ * <br/>
+ * <br/>
+ * Шаблон проектирования цепочки ответственности дает возможность нескольким объектам обрабатывать запрос,
+ * связывая принимающие объекты вместе в виде цепочки.
+ */
+public class ChainOfResponsibility {
+    public static void main(String[] args) {
+        AbstractLogger loggerChain = getChainOfLoggers();
+
+        loggerChain.logMessage(AbstractLogger.INFO,
+                "This is an information.");
+
+        loggerChain.logMessage(AbstractLogger.DEBUG,
+                "This is an debug level information.");
+
+        loggerChain.logMessage(AbstractLogger.ERROR,
+                "This is an error information.");
+    }
+
+    private static AbstractLogger getChainOfLoggers() {
+
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        return errorLogger;
+    }
+}
+
+abstract class AbstractLogger {
+
+    public static int INFO = 1;
+    public static int DEBUG = 2;
+    public static int ERROR = 3;
+
+    protected int level;
+
+    protected AbstractLogger nextLogger;
+
+    public void setNextLogger(AbstractLogger nextLogger) {
+        this.nextLogger = nextLogger;
+    }
+
+    public void logMessage(int level, String message) {
+        if (this.level <= level) {
+            write(message);
+        }
+        if (nextLogger != null) {
+            nextLogger.logMessage(level, message);
+        }
+    }
+
+    abstract protected void write(String message);
+}
+
+class ConsoleLogger extends AbstractLogger {
+
+    public ConsoleLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Standard Console::Logger: " + message);
+    }
+}
+
+class ErrorLogger extends AbstractLogger {
+
+    public ErrorLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Error Console::Logger: " + message);
+    }
+}
+
+class FileLogger extends AbstractLogger {
+
+    public FileLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
