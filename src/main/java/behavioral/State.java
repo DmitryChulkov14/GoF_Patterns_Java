5ed90bf0c653509e16f4a85d032620fccb0a9a1f
DmitryChulkov14 <dimon_krem@mail.ru>
2021-07-29 16:26:04 +0300

    State

---
+++ src/main/java/behavioral/State.java
@@ -0,0 +1,67 @@
+package behavioral;
+
+/**
+ * In State pattern a class behavior changes based on its state.
+ * <br/>
+ * <br/>
+ * В шаблоне состояния поведение класса изменяется в зависимости от его состояния.
+ */
+public class State {
+    public static void main(String[] args) {
+        StateContext context = new StateContext();
+
+        StartState startState = new StartState();
+        startState.doAction(context);
+
+        System.out.println(context.getState().toString());
+
+        StopState stopState = new StopState();
+        stopState.doAction(context);
+
+        System.out.println(context.getState().toString());
+    }
+}
+
+interface StateInterface {
+    void doAction(StateContext context);
+}
+
+class StartState implements StateInterface {
+
+    public void doAction(StateContext context) {
+        System.out.println("Player is in start state");
+        context.setState(this);
+    }
+
+    public String toString() {
+        return "Start State";
+    }
+}
+
+class StopState implements StateInterface {
+
+    public void doAction(StateContext context) {
+        System.out.println("Player is in stop state");
+        context.setState(this);
+    }
+
+    public String toString() {
+        return "Stop State";
+    }
+}
+
+class StateContext {
+    private StateInterface state;
+
+    public StateContext() {
+        state = null;
+    }
+
+    public void setState(StateInterface state) {
+        this.state = state;
+    }
+
+    public StateInterface getState() {
+        return state;
+    }
+}
